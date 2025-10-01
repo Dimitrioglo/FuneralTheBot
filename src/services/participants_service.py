f09f753c916e19b868974d57c668796b140113a5
Veaceslav Dimitrioglo <veaceslav.dimitrioglo-contractor@robertwalters.com>
2025-10-01 18:38:57 +0300

    feat: add logging

--- src/services/participants_service.py
+++ src/services/participants_service.py
@@ -3,6 +3,9 @@ from typing import Any
 from async_lru import alru_cache
 from telethon import TelegramClient
 
+from ioc.logging_container import LoggingContainer
+
+logger = LoggingContainer.logger()
 
 class ParticipantsService:
     def __init__(self, api_id: str, api_hash: str):
@@ -14,18 +17,21 @@ class ParticipantsService:
         self, chat_id: int, with_username: bool = True
     ) -> list[dict[str, Any]]:
         participants_list = []
-        async with TelegramClient(
-            "session", self.api_id, self.api_hash
-        ) as client:
-            async for user in client.iter_participants(chat_id):
-                if with_username and not user.username:
-                    continue
-                participants_list.append(
-                    {
-                        "id": user.id,
-                        "username": user.username,
-                        "full_name": user.first_name
-                        + (" " + user.last_name if user.last_name else ""),
-                    }
-                )
-        return participants_list
+        try:
+            async with TelegramClient(
+                "session", self.api_id, self.api_hash
+            ) as client:
+                async for user in client.iter_participants(chat_id):
+                    if with_username and not user.username:
+                        continue
+                    participants_list.append(
+                        {
+                            "id": user.id,
+                            "username": user.username,
+                            "full_name": user.first_name
+                            + (" " + user.last_name if user.last_name else ""),
+                        }
+                    )
+            return participants_list
+        except Exception as error:
+            logger.warning(error)
